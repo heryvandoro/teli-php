1df105144d2b997f57447eb4c1cea6bae20630d2
Hery Vandoro <vandorohery99@gmail.com>
2020-10-27 22:55:16 +0700

    enhance(common): remove api key from example

--- examples/client.php
+++ examples/client.php
@@ -4,4 +4,4 @@ include "../vendor/autoload.php";
 
 use Teli\Teli;
 
-$teli = new Teli('AC129fe07948e80fd295a90ea3b96381c4', 'e54deae4-40c7-43d7-940e-2c1171412d2b');
+$teli = new Teli('YOUR_CALL_API_SID_TOKEN', 'YOUR_API_TOKEN');
